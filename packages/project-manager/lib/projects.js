2bed6614e2eb1a1f28ed5294fd0f5be4cbf7bacf
Hung Vu <vuhung3990@gmail.com>
2016-04-05 05:01:20 +0700

    5Apr2016

---
+++ packages/project-manager/lib/projects.js
@@ -0,0 +1,60 @@
+'use babel';
+
+import {Emitter} from 'atom';
+import Project from './project';
+import db from './db';
+
+class Projects {
+  constructor() {
+    this.emitter = new Emitter();
+    this.projects = [];
+
+    db.addUpdater('iwantitall', {}, (project) => {
+      this.addProject(project);
+    });
+  }
+
+  onUpdate(callback) {
+    this.emitter.on('projects-updated', callback);
+  }
+
+  getAll(callback) {
+    db.find(projectSettings => {
+      for (const setting of projectSettings) {
+        this.addProject(setting);
+      }
+
+      callback(this.projects);
+    });
+  }
+
+  getCurrent(callback) {
+    this.getAll(projects => {
+      projects.forEach(project => {
+        if (project.isCurrent()) {
+          callback(project);
+        }
+      });
+    });
+  }
+
+  addProject(settings) {
+    let found = false;
+
+    for (const project of this.projects) {
+      if (project.props._id === settings._id) {
+        found = true;
+      } else if (project.rootPath === settings.paths[0]) {
+        found = true;
+      }
+    }
+
+    if (found === false) {
+      const newProject = new Project(settings);
+      this.projects.push(newProject);
+      this.emitter.emit('projects-updated');
+    }
+  }
+}
+
+export default new Projects();
---
+++ packages/project-manager/lib/save-dialog.coffee
@@ -0,0 +1,41 @@
+Dialog = require './dialog'
+Project = require './project'
+projects = require './projects'
+path = require 'path'
+
+module.exports =
+class SaveDialog extends Dialog
+  filePath: null
+
+  constructor: () ->
+    firstPath = atom.project.getPaths()[0]
+    title = path.basename(firstPath)
+    title = @prettifyTitle(title)
+
+    super
+      prompt: 'Enter name of project'
+      input: title
+      select: true
+      iconClass: 'icon-arrow-right'
+
+    projects.getCurrent (project) =>
+      if project.props.paths[0] is firstPath
+        @showError "This project is already saved as #{project.props.title}"
+
+
+  onConfirm: (title) ->
+    if title
+      properties =
+        title: title
+        paths: atom.project.getPaths()
+
+      project = new Project(properties)
+      project.save()
+
+      @close()
+    else
+      @showError('You need to specify a name for the project')
+
+  prettifyTitle: (title) ->
+    title = title.replace(/[^\w\s]/gi, ' ')
+    title = title.charAt(0).toUpperCase() + title.slice(1)
